18d7647fccd724c4500e6f988b84ce5a527e5de9
Chris Broome <chrisbroome7@gmail.com>
2015-01-12 11:29:24 -0500

    Fix topologicalSort when there are loops in the graph

--- test/fixtures.js
+++ test/fixtures.js
@@ -4,22 +4,55 @@ var
 
 module.exports = {
 
+  /**
+   * @return {Graph}
+   */
   test: function() {
+    return createBaseGraph();
+  },
+
+  /**
+   * @return {Graph}
+   */
+  testWithLoop: function() {
+    var graph = createBaseGraph();
+    var a = graph.getVertex('a');
+    // create a loop (node pointing to itself)
+    graph.addEdge(a, a);
+    return graph;
+  },
+
+  /**
+   * @return {Graph}
+   */
+  testWithCycles: function() {
     var graph = new Graph;
     var a = graph.addVertex('a');
     var b = graph.addVertex('b');
-    var c = graph.addVertex('c');
-    var d = graph.addVertex('d');
-    var e = graph.addVertex('e');
-    var f = graph.addVertex('f');
-    var g = graph.addVertex('g');
     graph.addEdge(a, b);
-    graph.addEdge(b, c);
-    graph.addEdge(d, c);
-    graph.addEdge(f, a);
-    graph.addEdge(g, e);
-    graph.addEdge(e, d);
+    graph.addEdge(b, a);
     return graph;
   }
 
 };
+
+/**
+ * @return {Graph}
+ */
+function createBaseGraph() {
+  var graph = new Graph;
+  var a = graph.addVertex('a');
+  var b = graph.addVertex('b');
+  var c = graph.addVertex('c');
+  var d = graph.addVertex('d');
+  var e = graph.addVertex('e');
+  var f = graph.addVertex('f');
+  var g = graph.addVertex('g');
+  graph.addEdge(a, b);
+  graph.addEdge(b, c);
+  graph.addEdge(d, c);
+  graph.addEdge(f, a);
+  graph.addEdge(g, e);
+  graph.addEdge(e, d);
+  return graph;
+}
--- test/graph.js
+++ test/graph.js
@@ -6,19 +6,43 @@ var
 module.exports = {
 
   'topologicalSort': {
-    'should sort': function() {
-      var
-        graph = fixtures.test(),
-        expected = [
-          { id: 'c' },
-          { id: 'b' },
-          { id: 'd' },
-          { id: 'a' },
-          { id: 'e' },
-          { id: 'f' },
-          { id: 'g' }
-        ];
-      assert.deepEqual(graph.topologicalSort(), expected);
+    'should return': {
+      'without cycles': function() {
+        var
+          graph = fixtures.test(),
+          expected = [
+            { id: 'c' },
+            { id: 'b' },
+            { id: 'd' },
+            { id: 'a' },
+            { id: 'e' },
+            { id: 'f' },
+            { id: 'g' }
+          ];
+        assert.deepEqual(graph.topologicalSort(), expected);
+      },
+      'with loops': function () {
+        var
+          graph = fixtures.testWithLoop(),
+          expected = [
+            { id: 'c' },
+            { id: 'b' },
+            { id: 'd' },
+            { id: 'a' },
+            { id: 'e' },
+            { id: 'f' },
+            { id: 'g' }
+          ];
+        assert.deepEqual(graph.topologicalSort(), expected);
+      }
+    },
+    'should throw': {
+      'with cycles': function() {
+        var graph = fixtures.testWithCycles();
+        assert.throws(function() {
+          graph.topologicalSort();
+        }, 'Cycle Detected');
+      }
     }
   }
 
